ef8f679700a1d4bcc699657e205379d55faaef98
RudranshSinghRaizada <harsh211109@gmail.com>
2025-04-28 19:24:31 +0530

    Add UnityGTA5Prototype/Scripts/ThirdPersonController.cs 1745848471520

---
+++ UnityGTA5Prototype/Scripts/ThirdPersonController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public float walkSpeed = 2f;
+    public float runSpeed = 6f;
+    public float jumpForce = 5f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") &amp;&amp; isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
